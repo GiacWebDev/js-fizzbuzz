39b22ce6c284d166e1e051c91eee32194d321231
GiacWebDev <giacobechini@gmail.com>
2023-09-11 15:49:40 +0200

    [EDIT] js cicles

--- JS/script.js
+++ JS/script.js
@@ -7,18 +7,26 @@
 // 3. identificare i multipli di 3 e appendere in html con "Fizz"
 // 4. identificare i multipli di 5 e appendere in html con "Buzz"
 
-// elementi
+// 1, elementi
 const listaE = document.querySelector('.lista');
 const liE = document.createElement('li')
 
-// 1.
+// 2.
 for (let i = 1; i <= 100; i++) {
   console.log(i);
 
-  // 2
-  if(i % 3 == 0) {
-    i = 'Fizz';
-    console.log('fizz');
+  // 3
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log('FizzBuzz');
+
+  } else if (i % 3 === 0) {
+    console.log('Fizz')
+
+  } else if (i % 5 === 0 ) {
+    console.log('Buzz')
+
+  } else {
+    console.log(i)
   }
 
 };
